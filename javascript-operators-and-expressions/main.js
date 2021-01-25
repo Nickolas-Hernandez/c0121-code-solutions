2bc69eab93725f74943edf54d97a22006d8c1a32
Nick Hernandez <xnickhernandez@gmail.com>
2021-01-25 12:43:53 -0800

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 10;
+var area = width * height;
+console.log('Value of area: ', area);
+console.log('Type of area: ', typeof area);
+
+var bill = 100;
+var payment = 120;
+var change = payment - bill;
+console.log('Value of change: ', change);
+console.log('Type of change: ', typeof change);
+
+var quizzes = 76;
+var midterm = 99;
+var final = 95;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade: ', grade);
+console.log('Type of grade: ', typeof grade);
+
+var firstName = 'Nick';
+var lastName = 'Hernandez';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName: ', fullName);
+console.log('Type of fullName: ', typeof fullName);
+
+var pH = 6.2;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic: ', isAcidic);
+console.log('Type of isAcidic: ', typeof isAcidic);
+
+var headCount = 222;
+var isSparta = headCount === 300;
+console.log('Value of isSparta: ', isSparta);
+console.log('Type of isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('Value of motto: ', motto);
+console.log('Type of motto: ', typeof motto);
