23f99071baf70902965eaa0cce1d891cccc315c1
Nick Hernandez <xnickhernandez@gmail.com>
2021-01-26 11:51:29 -0800

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,142 @@
+var orderHistory = [
+  {
+    orderDetails: {
+      orderPlaced: {
+        month: 'August',
+        day: 4,
+        year: 2020
+      },
+      orderTotal: 34,
+      orderNumber: '11439416898772232'
+    },
+    itemsPurchased: [
+      {
+        productName: 'Javascript for impatient programmers',
+        authorOrSeller: 'Rauschmayer, Dr. Axel',
+        price: 31.55,
+        returnBy: {
+          month: 'September',
+          date: 7,
+          year: 2020
+        }
+      }
+    ],
+    deliveryInfo: {
+      shipTo: 'JS Masher',
+      deliveredDate: {
+        month: 'August',
+        day: 8,
+        year: 2020,
+        handedToResident: false
+      }
+    }
+  },
+  {
+    orderDetails: {
+      orderPlaced: {
+        month: 'July',
+        day: 19,
+        year: 2020
+      },
+      orderTotal: 44.53,
+      orderNumber: '11399842681280257'
+    },
+    itemsPurchased: [
+      {
+        productName: 'The Timeless Way of Building',
+        authorOrSeller: 'Alexander, Christopher',
+        price: 41.33,
+        returnBy: {
+          month: 'August',
+          date: 19,
+          year: 2020
+        }
+      }
+    ],
+    deliveryInfo: {
+      shipTo: 'JS Masher',
+      deliveredDate: {
+        month: 'July',
+        day: 20,
+        year: 2020,
+        handedToResident: true
+      }
+    }
+  },
+  {
+    orderDetails: {
+      orderPlaced: {
+        month: 'July',
+        day: 4,
+        year: 2020
+      },
+      orderTotal: 17.22,
+      orderNumber: '11428755579059409'
+    },
+    itemsPurchased: [
+      {
+        productName: 'Gamecube Controller Adapter...',
+        authorOrSeller: '',
+        price: 15.98,
+        returnBy: {
+          month: 'August',
+          date: 5,
+          year: 2020
+        }
+      }
+    ],
+    deliveryInfo: {
+      shipTo: 'JS Masher',
+      deliveredDate: {
+        month: 'July',
+        day: 7,
+        year: 2020,
+        handedToResident: true
+      }
+    }
+  },
+  {
+    orderDetails: {
+      orderPlaced: {
+        month: 'July',
+        day: 3,
+        year: 2020
+      },
+      orderTotal: 138.93,
+      orderNumber: '11328831772648248'
+    },
+    itemsPurchased: [
+      {
+        productName: 'Gamecube Controller Adapter - Super Smash Bros. Edition (Nintendo Swithc)',
+        authorOrSeller: '',
+        price: 94.95,
+        returnBy: {
+          month: 'August',
+          date: 4,
+          year: 2020
+        }
+      },
+      {
+        productName: 'The Art of SQL',
+        authorOrSeller: 'Faroult, Stpehanie',
+        price: 33.99,
+        returnBy: {
+          month: 'August',
+          date: 4,
+          year: 2020
+        }
+      }
+    ],
+    deliveryInfo: {
+      shipTo: 'JS Masher',
+      deliveredDate: {
+        month: 'July',
+        day: 5,
+        year: 2020,
+        handedToResident: false
+      }
+    }
+  }
+];
+
+console.log('Value of order history: ', orderHistory);
