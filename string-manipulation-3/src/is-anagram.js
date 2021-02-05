487b4fc3e3394dfb2ef2cc089997803f40605962
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-05 10:01:48 -0800

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,9 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  for (var i = 0; i < firstString.length; i++) {
+    if (!secondString.includes(firstString[i])) {
+      return false;
+    }
+  }
+  return true;
+}
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,17 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var wordsArray = string.split(' ');
+  var reversedPhrase = '';
+  for (var i = 0; i < wordsArray.length; i++) {
+    var reversed = '';
+    for (var j = wordsArray[i].length - 1; j >= 0; j--) {
+      reversed += wordsArray[i][j];
+    }
+    if (i === wordsArray.length - 1) {
+      reversedPhrase += reversed;
+    } else {
+      reversedPhrase += reversed + ' ';
+    }
+  }
+  return reversedPhrase;
+}
