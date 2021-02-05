5bab17bfec3d8a78cbee5bd5a8c164e36909e178
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-05 15:43:34 -0800

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,20 @@
 /* exported titleCase */
+function titleCase(title){
+  var titleBreakdown = title.split(' ');
+  var casedTitle = [];
+  console.log("titleBreakdwn:", titleBreakdown);
+  for(var i = 0; i < titleBreakdown.length; i++){
+    if(titleBreakdown[i] === 'javascript:'){
+      casedTitle += 'JavaScript';
+    }else if(titleBreakdown[i].toLowerCase() === 'javascript'){
+      casedTitle += 'JavaScript';
+    }else if(titleBreakdown[i].toLowerCase() === 'api'){
+      casedTitle += 'API'
+    }else{
+     if(i === 0){
+        casedTitle[0] = titleBreakdown[i][0].toUpperCase() + titleBreakdown[i].slice(1);
+       console.log(casedTitle, "hi!");
+     }
+    }
+  }
+}
