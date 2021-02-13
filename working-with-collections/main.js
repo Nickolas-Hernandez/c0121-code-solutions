f71c552153c5e1c4b1d8c08dc63a1b8fec749619
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-13 15:27:13 -0800

    Completed working-with-collections

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,77 +1,88 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 var players = [
-  {
-    name: 'player1'
-  },
-  {
-    name: 'player2'
-  },
-  {
-    name: 'player3'
-  },
-  {
-    name: 'player4'
-  }
-]
-var suits = ['club', 'diamonds', 'hearts', 'spades'];
-var values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10,'jack', 'queen', 'king'];
+  { name: 'player1', hand: [] },
+  { name: 'player2', hand: [] },
+  { name: 'player3', hand: [] },
+  { name: 'player4', hand: [] }
+];
 
-function createDeck(){
-  var deck = [];
-  for(var i = 0; i < suits.length; i++){
-    for(var j=0; j < values.length; j++){
-      deck.push({suit: suits[i], value: values[j]});
+var deck = {
+  rank: ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'],
+  suit: ['club', 'diamond', 'hearts', 'spades']
+};
+
+function shuffleCards() {
+  var cardDeck = [];
+  for (var i = 0; i < deck.suit.length; i++) {
+    for (var j = 0; j < deck.rank.length; j++) {
+      var card = {};
+      card.rank = deck.rank[j];
+      card.suit = deck.suit[i];
+      cardDeck.push(card);
     }
   }
-  return _.shuffle(deck);
+  var shuffledDeck = _.shuffle(cardDeck);
+  return shuffledDeck;
 }
 
-function calcHandValue(player){
-  var handValue = 0;
-  var playerHand = player['hand'];
-  for(var j = 0; j < playerHand.length; j++){
-    if(playerHand[j]['value'] === 'ace'){
-      handValue += 11;
-    }else if(playerHand[j]['value'] === 'jack'|| playerHand[j]['value'] === 'queen'|| playerHand[j]['value'] === 'king'){
-      handValue += 10;
-    }else {
-      handValue += playerHand[j].value;
+function dealCards(playersArr, cardsPerHand, deck) {
+  var hands = _.chunk(deck, cardsPerHand);
+  for (var i = 0; i < playersArr.length; i++) {
+    playersArr[i].hand = hands.shift();
+  }
+}
+
+function calcValues(playersArr, handSize) {
+  for (var i = 0; i < playersArr.length; i++) {
+    var handValue = 0;
+    for (var j = 0; j < handSize; j++) {
+      if (playersArr[i].hand[j].rank === 'ace') {
+        handValue += 11;
+      } else if (playersArr[i].hand[j].rank === 'jack' || playersArr[i].hand[j].rank === 'queen' || playersArr[i].hand[j].rank === 'king') {
+        handValue += 10;
+      } else {
+        handValue += playersArr[i].hand[j].rank;
+      }
+      playersArr[i].value = handValue;
     }
   }
-  return handValue;
 }
 
-function decideWinner(arrayPLayers){
-  var playerValues = [];
-  for(var i = 0; i < arrayPLayers.length; i++){
-    playerValues.push(arrayPLayers[i]['handValue']);
+function findWinner(playersArr, handSize) {
+  var winner;
+  var scores = [];
+  var winnersBracket = [];
+  for (var i = 0; i < playersArr.length; i++) {
+    scores.push(playersArr[i].value);
   }
-  var highScore = _.max(playerValues);
-
-  console.log(playerValues);
-}
-
-
-function dealer(playersArr, cardsPerHand){
-  var newDeck = createDeck()
-  //create hands
-  var j = 0;
-  for(var i = 0; i < playersArr.length; i++){
-     var newHand = [];
-    while(newHand.length < cardsPerHand){
-      newHand.push(newDeck[j]);
-      j++;
+  var highScore = _.max(scores);
+  for (var j = 0; j < playersArr.length; j++) {
+    if (playersArr[j].value === highScore) {
+      winnersBracket.push(playersArr[j]);
     }
-    playersArr[i]['hand'] = newHand;
-    playersArr[i]['handValue'] = calcHandValue(playersArr[i]);
   }
-  //find winner
-  decideWinner(players);
-
-  console.log('newDeck:', newDeck);
-  console.log('players', playersArr);
-
+  if (winnersBracket.length > 1) {
+    for (var n = 0; n < winnersBracket.length; n++) {
+      winnersBracket[n].hand = [];
+    }
+    gameHandler(winnersBracket, handSize);
+  } else {
+    winner = winnersBracket[0].name;
+    return winner;
+  }
 }
 
-dealer(players, 2);
+function gameHandler(playersArr, cardsPerHand) {
+  var shuffledDeck = shuffleCards();
+  if (cardsPerHand > (shuffledDeck.length / playersArr.length)) {
+    console.log('Not enough cards! Run game again!');
+    return;
+  }
+  dealCards(playersArr, cardsPerHand, shuffledDeck);
+  calcValues(playersArr, cardsPerHand);
+  var winner = findWinner(playersArr, cardsPerHand);
+  console.log('winner!', winner);
+}
+
+gameHandler(players, 2);
