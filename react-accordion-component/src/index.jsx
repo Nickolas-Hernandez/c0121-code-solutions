ba13e588f51c22a5ba65c866dda8747011ec3c40
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-28 23:45:10 +0000

    Created array containing data for accordion component.

--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordian from './accordian';
 
+const topics = [
+  {
+    header: 'Hypertext Markup Language',
+    text: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.'
+  },
+  {
+    header: 'Cascading Style Sheets',
+    text: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.[2]'
+  },
+  {
+    header: 'JavaScript',
+    text: 'JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
+  }
+];
+
 ReactDOM.render(
-  <Accordian />,
+  <Accordian topics={topics} />,
   document.getElementById('root')
 );
