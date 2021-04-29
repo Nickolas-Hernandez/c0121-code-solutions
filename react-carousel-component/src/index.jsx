fc5b552973985f4c729517679b7ca23218c5ec70
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-29 22:07:36 +0000

    Created data array  containing image urls and alt text. Passed array to carousel via props.

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './carousel.jsx';
 
+const imageURLs = [
+  {
+    relativePath: '../images/001.png',
+    alt: 'Bulbasaur'
+  },
+  {
+    relativePath: '../images/004.png',
+    alt: 'Charmander'
+  },
+  {
+    relativePath: '../images/007.png',
+    alt: 'Squirtle'
+  },
+  {
+    relativePath: '../images/025.png',
+    alt: 'Pikachu'
+  },
+  {
+    relativePath: '../images/039.png',
+    alt: 'Jiggly Puff'
+  }
+];
+
 ReactDOM.render(
-  <Carousel />, document.getElementById('#root')
+  <Carousel imagesData={imageURLs} />, document.getElementById('#root')
 );
