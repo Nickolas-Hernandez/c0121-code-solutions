959be4d68c46e7b259e32222676f08d6e9dc7da2
Nick Hernandez <xnickhernandez@gmail.com>
2021-04-29 23:13:23 +0000

    Added keys to data object.

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -4,22 +4,27 @@ import Carousel from './carousel';
 
 const imageURLs = [
   {
+    key: 1,
     relativePath: '../images/001.png',
     alt: 'Bulbasaur'
   },
   {
+    key: 2,
     relativePath: '../images/004.png',
     alt: 'Charmander'
   },
   {
+    key: 3,
     relativePath: '../images/007.png',
     alt: 'Squirtle'
   },
   {
+    key: 4,
     relativePath: '../images/025.png',
     alt: 'Pikachu'
   },
   {
+    key: 5,
     relativePath: '../images/039.png',
     alt: 'Jiggly Puff'
   }
